77aa5faec0aef8eaf1a870fd5e5d4a3207f8971a
Oumaima Dagoun <121551204+oumaimadgn@users.noreply.github.com>
2024-12-08 14:27:29 +0100

    Update GrpcApplication.java

--- src/main/java/ma/emsi/grpc/GrpcApplication.java
+++ src/main/java/ma/emsi/grpc/GrpcApplication.java
@@ -20,27 +20,13 @@ public class GrpcApplication {
     @Bean
     CommandLineRunner start(CompteRepository compteRepository) {
         return args -> {
-            compteRepository.save(new Compte(null, 1000.0, new Date(2022 - 1900, 1, 15), TypeCompte.EPARGNE));
-            compteRepository.save(new Compte(null, 2500.0, new Date(2022 - 1900, 2, 10), TypeCompte.COURANT));
-            compteRepository.save(new Compte(null, 3200.0, new Date(2022 - 1900, 3, 5), TypeCompte.EPARGNE));
-            compteRepository.save(new Compte(null, 4200.0, new Date(2022 - 1900, 4, 20), TypeCompte.COURANT));
-            compteRepository.save(new Compte(null, 1500.0, new Date(2022 - 1900, 5, 25), TypeCompte.EPARGNE));
-            compteRepository.save(new Compte(null, 5400.0, new Date(2023 - 1900, 6, 10), TypeCompte.COURANT));
-            compteRepository.save(new Compte(null, 6300.0, new Date(2023 - 1900, 7, 15), TypeCompte.EPARGNE));
-            compteRepository.save(new Compte(null, 7200.0, new Date(2023 - 1900, 8, 20), TypeCompte.COURANT));
-            compteRepository.save(new Compte(null, 8200.0, new Date(2023 - 1900, 9, 5), TypeCompte.EPARGNE));
-            compteRepository.save(new Compte(null, 9100.0, new Date(2023 - 1900, 10, 30), TypeCompte.COURANT));
-            compteRepository.save(new Compte(null, 4500.0, new Date(2024 - 1900, 0, 10), TypeCompte.EPARGNE));
-            compteRepository.save(new Compte(null, 2800.0, new Date(2024 - 1900, 1, 15), TypeCompte.COURANT));
-            compteRepository.save(new Compte(null, 3600.0, new Date(2024 - 1900, 2, 20), TypeCompte.EPARGNE));
-            compteRepository.save(new Compte(null, 4800.0, new Date(2024 - 1900, 3, 25), TypeCompte.COURANT));
-            compteRepository.save(new Compte(null, 5800.0, new Date(2024 - 1900, 4, 30), TypeCompte.EPARGNE));
-            compteRepository.save(new Compte(null, 6700.0, new Date(2024 - 1900, 5, 5), TypeCompte.COURANT));
-            compteRepository.save(new Compte(null, 7500.0, new Date(2024 - 1900, 6, 10), TypeCompte.EPARGNE));
-            compteRepository.save(new Compte(null, 8300.0, new Date(2024 - 1900, 7, 15), TypeCompte.COURANT));
-            compteRepository.save(new Compte(null, 9200.0, new Date(2024 - 1900, 8, 20), TypeCompte.EPARGNE));
-            compteRepository.save(new Compte(null, 9900.0, new Date(2024 - 1900, 9, 25), TypeCompte.COURANT));
-        };
+            compteRepository.save(new Compte(null, 1100.0, new Date(2024 - 1900, 10, 5), TypeCompte.EPARGNE));
+            compteRepository.save(new Compte(null, 2300.0, new Date(2024 - 1900, 11, 10), TypeCompte.COURANT));
+            compteRepository.save(new Compte(null, 3400.0, new Date(2025 - 1900, 0, 15), TypeCompte.EPARGNE));
+            compteRepository.save(new Compte(null, 4100.0, new Date(2025 - 1900, 1, 20), TypeCompte.COURANT));
+            compteRepository.save(new Compte(null, 5200.0, new Date(2025 - 1900, 2, 25), TypeCompte.EPARGNE));
+            compteRepository.save(new Compte(null, 6000.0, new Date(2025 - 1900, 3, 5), TypeCompte.COURANT));
+            };
     }
 
 }
